4720e1b4e91847862b698bef46832a8be1eb063e
vipulj <jayanthgowda2000@gmail.com>
2022-10-04 09:34:01 +0530

    Training exercices

---
+++ InheritaNce.java
@@ -0,0 +1,87 @@
+ class InheritaNce {
+        private String customerId;
+        private String customerName;
+
+        public InheritaNce() {
+            System.out.println("Parent Default Constructor");
+        }
+
+        public InheritaNce(String customerId, String customerName) {
+            System.out.println("from param con");
+            this.customerId = customerId;
+            this.customerName = customerName;
+        }
+
+        public String getCustomerId() {
+            return customerId;
+        }
+
+        public void setCustomerId(String customerId) {
+            this.customerId = customerId;
+        }
+
+        public String getCustomerName() {
+            return customerName;
+        }
+
+        public void setCustomerName(String customerName) {
+            this.customerName = customerName;
+        }
+
+        public void displayCustomerDetails() {
+            System.out.println("Displaying customer details \n***************************");
+            System.out.println("Customer Id : " + customerId);
+            System.out.println("Customer Name : " + customerName);
+            System.out.println();
+        }
+    }
+
+
+    class RegularCustomer extends InheritaNce   {
+        private float discount;
+
+        public RegularCustomer(String custId, String custName) {
+            super(custId, custName);
+            this.discount = 5.0f;
+            System.out.println("Child Constructor");
+        }
+
+        public float getDiscount() {
+            return discount;
+        }
+
+        public void setDiscount(float discount) {
+            this.discount = discount;
+        }
+
+        public static void main(String[] args) {
+            RegularCustomer regularCustomer = new RegularCustomer("C1010", "Johns Kora");
+            regularCustomer.displayCustomerDetails();
+        }
+
+    }
+
+    class A2 {
+        A2(int a) {
+            System.out.println("from A2 cons");
+        }
+
+    }
+
+    class B2 extends A2 {
+        B2() {
+            super(5);
+//       this(6);
+            System.out.println("from B2");
+        }
+
+        B2(int a) {
+            super(5);
+            System.out.println("from parameter");
+        }
+
+        public static void main(String[] args) {
+            B2 obj = new B2();
+        }
+    }
+
